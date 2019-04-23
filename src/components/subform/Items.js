8a16f24931c24793e34e2295b7b0e67fa4bff2af
johanlef <jb@eenwereldmetlef.be>
2019-04-23 10:26:55 +0200

    Improve subform itemlist
    [LP-3] #close
    
    - Change order of items
    
    - Duplicate items (opens form after duplication)
    
    - use icons for buttons with text in title attribute (native tooltip)

--- src/components/subform/Items.js
+++ src/components/subform/Items.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table, Button } from 'reactstrap'
+import { Table, Button, ButtonGroup } from 'reactstrap'
 
 import { translatorText } from '../../helpers/translator'
 
@@ -8,45 +8,107 @@ const columns = ({ elements, attributes }) =>
     ? attributes.columns
     : elements.map(({ name }) => name) || []
 
-export default ({ items, remove, edit, element, translator }) => (
-  <Table>
-    <thead>
-      <tr>
-        <th>#</th>
-        {columns(element).map((col, i) => (
-          <th key={i}>{translatorText(col, translator)}</th>
-        ))}
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      {(items || []).map((d, i) => (
-        <tr key={i}>
-          <td>{1 + i}.</td>
-          {columns(element).map((col, j) => (
-            <td key={`${i}.${j}`}>{d[col].toString()}</td>
+export default ({
+  items,
+  remove,
+  edit,
+  element,
+  translator,
+  move,
+  duplicate
+}) => {
+  const canMove = (index, dir) =>
+    dir < 0 ? index > 0 : index < items.length - 1
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          {columns(element).map((col, i) => (
+            <th key={i}>{translatorText(col, translator)}</th>
           ))}
-          <td className={'text-right text-nowrap'}>
-            <Button
-              color={'danger'}
-              size={'sm'}
-              outline
-              onClick={() => remove(i)}
-            >
-              {translatorText(
-                { nl: 'Verwijderen', fr: 'Supprimer', en: 'Remove' },
-                translator
-              )}
-            </Button>{' '}
-            <Button color={'info'} size={'sm'} outline onClick={() => edit(i)}>
-              {translatorText(
-                { nl: 'Wijzigen', fr: 'Changer', en: 'Edit' },
-                translator
-              )}
-            </Button>
-          </td>
+          <th />
         </tr>
-      ))}
-    </tbody>
-  </Table>
-)
+      </thead>
+      <tbody>
+        {items.map((d, i) => (
+          <tr key={i}>
+            <td style={{ verticalAlign: 'middle' }}>{1 + i}.</td>
+            {columns(element).map((col, j) => (
+              <td style={{ verticalAlign: 'middle' }} key={`${i}.${j}`}>
+                {d[col].toString()}
+              </td>
+            ))}
+            <td className={'text-right text-nowrap'}>
+              <ButtonGroup>
+                <Button
+                  color={'warning'}
+                  size={'sm'}
+                  outline
+                  onClick={() => edit(i)}
+                  title={translatorText(
+                    { nl: 'Wijzigen', fr: 'Changer', en: 'Edit' },
+                    translator
+                  )}
+                >
+                  ✎
+                </Button>
+                <Button
+                  color={'danger'}
+                  size={'sm'}
+                  outline
+                  onClick={() => remove(i)}
+                  disabled={!remove}
+                  title={translatorText(
+                    { nl: 'Verwijderen', fr: 'Supprimer', en: 'Remove' },
+                    translator
+                  )}
+                >
+                  ✕
+                </Button>
+                <Button
+                  color={'success'}
+                  size={'sm'}
+                  outline
+                  onClick={() => duplicate(i)}
+                  title={translatorText(
+                    { nl: 'Dupliceren', fr: 'Copier', en: 'Copy' },
+                    translator
+                  )}
+                >
+                  ⧉
+                </Button>
+                <Button
+                  color={'info'}
+                  size={'sm'}
+                  outline
+                  onClick={() => move(i, -1)}
+                  disabled={!canMove(i, -1)}
+                  title={translatorText(
+                    { nl: 'Omhoog', fr: 'En haut', en: 'Move up' },
+                    translator
+                  )}
+                >
+                  △
+                </Button>
+                <Button
+                  color={'info'}
+                  size={'sm'}
+                  outline
+                  onClick={() => move(i, 1)}
+                  disabled={!canMove(i, 1)}
+                  title={translatorText(
+                    { nl: 'Omlaag', fr: 'En bas', en: 'Move down' },
+                    translator
+                  )}
+                >
+                  ▽
+                </Button>
+              </ButtonGroup>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
--- src/components/subform/Subform.js
+++ src/components/subform/Subform.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { get, castArray } from 'lodash'
+import { get, castArray, clone, cloneDeep } from 'lodash'
 import { Row, Col, Card, CardHeader, CardBody, Button } from 'reactstrap'
 
 import ActualForm from './Modal'
@@ -16,6 +16,8 @@ class SubForm extends React.Component {
     }
     this.removeItem = this.removeItem.bind(this)
     this.saveItem = this.saveItem.bind(this)
+    this.moveItem = this.moveItem.bind(this)
+    this.duplicateItem = this.duplicateItem.bind(this)
     this.toggleForm = this.toggleForm.bind(this)
     this._openForm = this._openForm.bind(this)
     this._closeForm = this._closeForm.bind(this)
@@ -38,16 +40,53 @@ class SubForm extends React.Component {
   _getValue () {
     const { bindInput, element } = this.props
     const { name, value, onChange } = bindInput(element.name)
-    const data = value ? castArray(value) : []
+    const data = value ? cloneDeep(castArray(value)) : []
     return { name, value: data, onChange }
   }
-  _modifyModel (index = 0, model) {
+  _modifyModel (action, { index, model, direction }) {
     const { name, value, onChange } = this._getValue()
-    const isNew = index < 0
-    const i = isNew ? value.length : index
-    model
-      ? value.splice(i, isNew ? 0 : 1, model)
-      : value.splice(i, isNew ? 0 : 1)
+    switch (
+      action // CRUD naming + move + duplicate
+    ) {
+      case 'create':
+        if (model === undefined) {
+          console.warn('This action requires the options {model} to be set')
+        }
+        value.splice(value.length, 0, model)
+        break
+      case 'update':
+        if (index < 0 || model === undefined) {
+          console.warn(
+            'This action requires the options {model, index} to be set'
+          )
+        }
+        value.splice(index, 1, model)
+        break
+      case 'delete':
+        if (index < 0) {
+          console.warn('This action requires the options {index} to be set')
+        }
+        value.splice(index, 1)
+        break
+      case 'move':
+        if (index < 0 || direction === undefined || model === undefined) {
+          console.warn(
+            'This action requires the options {index, direction, model} to be set'
+          )
+        }
+        value.splice(index, 1)
+        value.splice(index + direction, 0, model)
+        break
+      case 'duplicate':
+        const newModel = value[index]
+        if (index < 0 || newModel === undefined) {
+          console.warn(
+            'This action requires the options {index} to be set and a valid source'
+          )
+        }
+        value.splice(1 + index, 0, newModel)
+        break
+    }
     this.setState({ data: value }, () => onChange({ target: { name, value } })) // onChange event
     return value
   }
@@ -62,12 +101,22 @@ class SubForm extends React.Component {
       this.props.translator
     )
     const confirmed = confirm(removeText)
-    if (value[index] && confirmed) this._modifyModel(index)
+    if (value[index] && confirmed) this._modifyModel('delete', { index })
   }
-  saveItem (submodel) {
+  saveItem (model) {
     const { currentIndex } = this.state
-    this._modifyModel(currentIndex, submodel)
+    if (currentIndex >= 0) {
+      this._modifyModel('update', { index: currentIndex, model })
+    } else this._modifyModel('add', { model })
     this._closeForm()
+  }
+  duplicateItem (index) {
+    this._modifyModel('duplicate', { index })
+    this.toggleForm(1 + index)
+  }
+  moveItem (index, direction) {
+    const value = clone(this._getValue().value[index])
+    if (value) this._modifyModel('move', { index, direction, model: value })
   }
   render () {
     const { element, translator } = this.props
@@ -109,8 +158,10 @@ class SubForm extends React.Component {
         <CardBody className={'small'}>
           <Items
             items={data}
-            remove={this.removeItem}
+            remove={data.length > min ? this.removeItem : false}
             edit={this.toggleForm}
+            move={this.moveItem}
+            duplicate={this.duplicateItem}
             element={element}
             translator={translator}
           />
