c4401c4898d437ed66ba8d7ac506cd2ccb8c619b
johanlef <jb@eenwereldmetlef.be>
2019-04-25 15:57:53 +0200

    Upgrade built-in markdown component
    [KCESOA-109]

--- src/helpers/Text.js
+++ src/helpers/Text.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import MarkdownIt from 'markdown-it'
+import MarkdownItAttrs from 'markdown-it-attrs'
+import MarkdownItVideo from 'markdown-it-video'
+
+const markdown = MarkdownIt({
+  html: true,
+  linkify: true,
+  typography: true
+})
+  .use(MarkdownItAttrs)
+  .use(MarkdownItVideo)
 
 const Text = ({ content, className }) => {
   const md = new MarkdownIt()
