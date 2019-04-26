1f9e52cbd8ed169d41af9fb4944d72481c0b0ea3
johanlef <jb@eenwereldmetlef.be>
2019-04-26 10:51:38 +0200

    setup gh-pages

--- src/components/Divider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DividerComponent = props => {
-  const { element, attributes: propsAttributes } = props
-  const { attributes: elementAttributes } = element
-  return <hr {...elementAttributes} {...propsAttributes} />
-}
-
-export default DividerComponent
