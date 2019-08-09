937801f7422540220a9824137f8a22fcc013a70e
laerts <la@eenwereldmetlef.be>
2019-08-09 10:46:42 +0200

    Fix Translator text

--- src/helpers/translator.js
+++ src/helpers/translator.js
@@ -4,13 +4,21 @@ import { isString, get, head, map } from 'lodash'
 const translatorText = (text, translator, getDefault, getString) => {
   if (!text) return ''
   if (isString(text)) return text
-  if (translator.component && text.translate) {
-    return <translator.component _id={text.translate} getString={getString} />
-  }
+
   const lang = getDefault
     ? get(translator, 'default', get(translator, 'currentLanguage', 'default'))
     : get(translator, 'currentLanguage', get(translator, 'default', 'default'))
-  return text[lang] || text.default || text.translate || head(map(text))
+  return (
+    text[lang] ||
+    text.default ||
+    (translator.component && text.translate ? (
+      <translator.component _id={text.translate} getString={getString} />
+    ) : (
+      false
+    )) ||
+    text.translate ||
+    head(map(text))
+  )
 }
 
 export { translatorText }
