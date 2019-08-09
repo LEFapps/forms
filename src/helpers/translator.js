63a48eaa6c00496c243ba56d746a10b7a3214f43
johanlef <jb@eenwereldmetlef.be>
2019-08-09 08:57:24 +0200

    Prioritise translations in text helper

--- src/helpers/translator.js
+++ src/helpers/translator.js
@@ -2,30 +2,15 @@ import React from 'react'
 import { isString, get, head, map } from 'lodash'
 
 const translatorText = (text, translator, getDefault, getString) => {
-  if (text) {
-    if (isString(text)) return text
-    const lang = getDefault
-      ? get(
-        translator,
-        'default',
-        get(translator, 'currentLanguage', 'default')
-      )
-      : get(
-        translator,
-        'currentLanguage',
-        get(translator, 'default', 'default')
-      )
-    return (
-      text[lang] ||
-      text.default ||
-      (translator.component ? (
-        <translator.component _id={text.translate} getString={getString} />
-      ) : (
-        false
-      )) ||
-      head(map(text))
-    )
-  } else return ''
+  if (!text) return ''
+  if (isString(text)) return text
+  if (translator.component && text.translate) {
+    return <translator.component _id={text.translate} getString={getString} />
+  }
+  const lang = getDefault
+    ? get(translator, 'default', get(translator, 'currentLanguage', 'default'))
+    : get(translator, 'currentLanguage', get(translator, 'default', 'default'))
+  return text[lang] || text.default || text.translate || head(map(text))
 }
 
 export { translatorText }
